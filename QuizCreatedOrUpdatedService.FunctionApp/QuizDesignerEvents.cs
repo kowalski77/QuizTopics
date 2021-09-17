963814cf308c2ab64bbeb6bdc918588de70902fd
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2021-09-17 18:31:33 +0200

    Add Evaluator API

--- QuizCreatedOrUpdatedService.FunctionApp/QuizDesignerEvents.cs
+++ QuizCreatedOrUpdatedService.FunctionApp/QuizDesignerEvents.cs
@@ -7,7 +7,7 @@ namespace QuizCreatedOrUpdatedService.FunctionApp
     {
         public sealed record QuizCreated(Guid Id, string Name, string Exam, IEnumerable<ExamQuestion> ExamQuestionCollection);
 
-        public sealed record ExamQuestion(string Text, IEnumerable<ExamAnswer> ExamAnswerCollection);
+        public sealed record ExamQuestion(string Text, string Tag, int Difficulty, IEnumerable<ExamAnswer> ExamAnswerCollection);
 
         public sealed record ExamAnswer(string Text, bool IsCorrect);
     }
