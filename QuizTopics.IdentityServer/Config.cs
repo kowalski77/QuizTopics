8f15665891b0681322f69655f3faef2ce01fa566
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2021-09-22 14:00:23 +0200

    qqq

---
+++ QuizTopics.IdentityServer/Config.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace QuizTopics.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new("candidateapi", "Generic access")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new()
+                {
+                    ClientId = "candidateclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5005/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5005/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "candidateapi"
+                    }
+                }
+            };
+    }
+}
---
+++ QuizTopics.IdentityServer/Startup.cs
@@ -0,0 +1,68 @@
+using Duende.IdentityServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace QuizTopics.IdentityServer
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential(); 
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    options.ClientId = "844964698772-jn8f8fp3k1fq7ic8gb9dvr8khfpmto7l.apps.googleusercontent.com";
+                    options.ClientSecret = "JYUo4p6Y1_BjhJhsHzC4JVtP";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
