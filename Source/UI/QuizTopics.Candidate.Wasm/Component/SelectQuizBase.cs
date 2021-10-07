bd751adb296725077342e9ab47dbc1c849bebadf
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2021-10-07 18:42:08 +0200

    select

--- Source/UI/QuizTopics.Candidate.Wasm/Component/SelectQuizBase.cs
+++ Source/UI/QuizTopics.Candidate.Wasm/Component/SelectQuizBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using QuizTopics.Candidate.Wasm.Services;
@@ -8,19 +10,30 @@ namespace QuizTopics.Candidate.Wasm.Component
 {
     public class SelectQuizBase : ComponentBase
     {
+        private Guid selectedQuizViewModel = Guid.Empty;
+        private bool isButtonEnabled;
+
         [Inject] private IQuizDataService QuizDataService { get; set; }
 
         protected IEnumerable<QuizViewModel> QuizViewModelCollection { get; private set; }
 
+        protected string ButtonClass => this.isButtonEnabled ? "input-group-text" : "input-group-text disabled";
+
         protected override async Task OnInitializedAsync()
         {
             var result = await this.QuizDataService.GetAsync();
-            this.QuizViewModelCollection = result.Value;
+            this.QuizViewModelCollection = result.Value.ToList();
         }
 
-        protected void OnSelectedValueChanged()
+        protected void OnSelectedValueChanged(Guid id)
         {
+            this.selectedQuizViewModel = id;
+            this.isButtonEnabled = this.selectedQuizViewModel != Guid.Empty;
+        }
 
+        protected Task StartAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
